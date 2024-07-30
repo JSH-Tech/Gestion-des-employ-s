df64a8e0a0a01d658f108d75d054a22612b75705
JSH-Tech <josueabots@gmail.com>
2024-07-30 02:58:55 -0400

    Fin implementation interface Ipaiement

---
+++ IPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaoJosueAbotsidiaUA3Projet
+{
+    public interface IPayment
+    {
+        public abstract double CalculatePayment();
+    }
+}
